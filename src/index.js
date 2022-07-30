8e51cd0e693bb6b9c3d28cb5e1508b70a178d1c8
mabilweb <h3mabil@gmail.com>
2022-07-30 12:27:50 -0500

    cambio61

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const path =require('path');
 //settings
 app.set('port',process.env.PORT || 4000);
 app.set('views',path.join(__dirname,'views'));
-//app.set('view engine', 'ejs');
+app.set('view engine', 'ejs');
 
 
 //app.set('views',path.join(__dirname,'views'));
