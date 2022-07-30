70a4adbc676ae999b6af71e67833bd5c65d27a4d
mabilweb <h3mabil@gmail.com>
2022-07-30 12:08:58 -0500

    cambio59

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 const express = require('express');
 const app =express();
 const path =require('path');
+const exphbs=require('express-handlebars');
 
 //settings
 app.set('port',process.env.PORT || 4000);
 app.set('views',path.join(__dirname,'views'));
-app.set('view engine', 'ejs');
+//app.set('view engine', 'ejs');
+
+
+//app.set('views',path.join(__dirname,'views'));
+//app.engine('.hbs',exphbs({
+//defaultLayout:'main',
+//layoutsDir:path.join(app.get('views'),'layouts'),
+//partialsDir:path.join(app.get('views'),'partials'),
+//extname:'.hbs',
+//helpers:require('./lib/handlebars')
+//}));
+app.set('view engine','.hbs');
 //middleawres
+
+
+
+
 ///routes
 app.use(require('./routes/index'));
 ///static files
