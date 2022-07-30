25ba716329a4cc9d7374076f335f85e64746f007
mabilweb <h3mabil@gmail.com>
2022-07-29 21:19:52 -0500

    cambio47

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,9 @@ const express =require('express');
 const router = express.Router();
 
 router.get('/',(req,res)=>{
+    //res.sendFile(path.join(__dirname,'views/index.html'));
     res.render('index');
 });
 
-module.exports=router;
+
+module.exports = router;
