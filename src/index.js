cbd6eb39078de167a7e7ac9a0f84995e0fff720a
mabilweb <h3mabil@gmail.com>
2022-07-30 11:45:47 -0500

    cambio57

--- src/index.js
+++ src/index.js
@@ -1,36 +1,18 @@
-const express =require('express');
-const morgan =require('morgan');
-const exphbs=require('express-handlebars');
+const express = require('express');
+const app =express();
 const path =require('path');
 
-const app=express();
-
 //settings
-app.set('port',process.env.PORT ||4000);
+app.set('port',process.env.PORT || 4000);
 app.set('views',path.join(__dirname,'views'));
-app.engine('.hbs',exphbs({
-defaultLayout:'main',
-layoutsDir:path.join(app.get('views'),'layouts'),
-partialsDir:path.join(app.get('views'),'partials'),
-extname:'.hbs',
-helpers:require('./lib/handlebars')
-}));
-app.set('view engine','.hbs');
+app.set('view engine', 'ejs');
+//middleawres
+///routes
+app.use(require('./routes/index'));
+///static files
+app.use(express.static(__dirname + '/public'));
 
-//middlewares
-
-//global varials
-app.use(morgan('dev'));
-
-//Routes
-app.use(require('./routes'));
-
-//Public
-//app.use(express.static('public'))
-app.use(express.static(path.join(__dirname,'public')));
-//app.use(express.static(path.join(__dirname,'public')));
-
-//statir server
-app.listen(app.get('port'),()=>{
-    console.log('server on pedro',app.get('port'));
+///listeig the server
+app.listen(app.get('port'), () =>{
+    console.log('sirviendo  en :', app.get('port'));
 });
