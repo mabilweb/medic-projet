0436abcbf2f5e7ed24d63736dfe13b23a9c71662
mabilweb <h3mabil@gmail.com>
2022-07-30 11:27:21 -0500

    cambio56

--- src/index.js
+++ src/index.js
@@ -2,14 +2,9 @@ const express =require('express');
 const morgan =require('morgan');
 const exphbs=require('express-handlebars');
 const path =require('path');
-const flash=require('connect-flash');
-const session =require('express-session');
-const MySQLStore=require('express-mysql-session');
-const passport = require('passport');
 
-const {database}=require('./keys');
 const app=express();
-require('./lib/passport');
+
 
 //settings
 app.set('port',process.env.PORT ||4000);
@@ -27,29 +22,10 @@ app.set('view engine','.hbs');
 
 //global varials
 app.use(morgan('dev'));
-app.use(express.urlencoded({extended:false}));
-app.use(express.json());
-app.use(session({
-    secret:'faztmysqlnodesession',
-    resave:false,
-    saveUninitialized:false,
-    store: new MySQLStore(database)
-}));
-app.use(flash());
-app.use(passport.initialize());
-app.use(passport.session());
 
 
-app.use((req,res,next)=>{
-    app.locals.success=req.flash('success');
-    app.locals.message=req.flash('message');
-    app.locals.user = req.user;
-    next();
-    });
 //Routes
 app.use(require('./routes'));
-app.use(require('./routes/autenthication'));
-app.use('/links',require('./routes/links'));
 
 //Public
 //app.use(express.static('public'))
