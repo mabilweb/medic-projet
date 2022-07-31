8bbd5c8a4b13c91a6ad0f0554760e91e38875fde
mabilweb <h3mabil@gmail.com>
2022-07-31 05:53:32 -0500

    cambio27

--- index.js
+++ index.js
@@ -2,23 +2,25 @@ const express =require('express');
 const exphbs=require('express-handlebars');
 const morgan =require('morgan');
 const path =require('path');
+const hbs = require('hbs');
 const app =express();
 
 //settings
 app.set('port',process.env.PORT || 5000);
-app.set('views',path.join(__dirname,'views'));
-app.set('view engine', 'ejs');
-
-
 //app.set('views',path.join(__dirname,'views'));
-//app.engine('.hbs',exphbs({
-//defaultLayout:'main',
-////layoutsDir:path.join(app.get('views'),'layouts'),
-////partialsDir:path.join(app.get('views'),'partials'),
-//extname:'.hbs'
-////helpers:require('./lib/handlebars')
-//}));
+//app.set('view engine', 'ejs');
 //app.set('view engine','.hbs');
+
+
+app.set('views',path.join(__dirname,'views'));
+app.engine('.hbs',exphbs({
+defaultLayout:'main',
+//layoutsDir:path.join(app.get('views'),'layouts'),
+//partialsDir:path.join(app.get('views'),'partials'),
+extname:'.hbs'
+//helpers:require('./lib/handlebars')
+}));
+app.set('view engine','.hbs');
 //middleawres
 
 ///routes
